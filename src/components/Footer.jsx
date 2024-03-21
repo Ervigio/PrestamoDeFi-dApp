68fd18e942f655da5e86e29303650a2adb504c23
Ervigio <ejadd_75@hotmail.com>
2024-03-21 22:49:03 +0000

    Aplicar estilso de prueba

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="border-t py-4 px-3 flex justify-center bg-red-400 shadow-xs">
+    <footer className="border-t py-4 px-3 flex justify-center bg-gradient-to-r from-orange-400 to-pink-600 shadow-xs">
       <p>
         Derechos de autor &copy;{new Date().getFullYear()} Blockmaker Academy
       </p>
