7616ce96d97514faed365b785f0da624ab799f6b
Ervigio <ejadd_75@hotmail.com>
2024-03-21 22:46:40 +0000

    Arreglar errores de sintaxis exportaciones/importaciones componentes Navbar y Home y eliminar componente Navbar donde no procede

--- src/pages/Pagina1/Home.jsx
+++ src/pages/Pagina1/Home.jsx
@@ -1,9 +1,7 @@
-import { Navbar } from "../../components/Navbar";
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline ">Componente Home</h1>
-      <Navbar />
     </div>
   );
 }
--- src/pages/Pagina3/OperacioneDePrestamos.jsx
+++ src/pages/Pagina3/OperacioneDePrestamos.jsx
@@ -1,10 +1,7 @@
-import {Navbar} from "../../components/Navbar"
-
 export default function OperacioneDePrestamos() {
   return (
     <div>
       <h3>Ésta es la Página 3</h3>
-      <Navbar/>
     </div>
   );
 }
