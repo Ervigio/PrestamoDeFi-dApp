c9b768c2b0a4918b046d064b720c3e68127509ec
Ervigio <ejadd_75@hotmail.com>
2024-03-21 22:50:26 +0000

    Aplicar estilo de prueba componente Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className=" ">
+      <ul className="flex ">
+        <li className="px-4 ">
           <Link to="/src/pages/Pagina1/">Inicio y Autenticación</Link>
         </li>
-        <li>
+        <li className="px-4  ">
           <Link to="/src/pages/Pagina2">Gestión de Usuarios</Link>
         </li>
-        <li>
+        <li className="px-4 ">
           <Link to="/src/pages/Pagina3">Operaciones de Préstamos</Link>
         </li>
       </ul>
