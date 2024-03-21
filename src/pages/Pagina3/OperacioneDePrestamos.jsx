49e06ccf376863d5b91172e3445c2a1010ddbe17
Ervigio <ejadd_75@hotmail.com>
2024-03-21 01:07:50 +0000

    Reorganizar Carpeta Pages: - añadiendo subcarpeta Pagina3 incluyendo componentes OperacionesDePrestamos.jsx e índice

---
+++ src/pages/Pagina3/OperacioneDePrestamos.jsx
@@ -0,0 +1,7 @@
+export default function OperacioneDePrestamos() {
+  return (
+    <div>
+      <h3>Ésta es la Página 3</h3>
+    </div>
+  );
+}
---
+++ src/pages/Pagina3/index.js
@@ -0,0 +1,1 @@
+export { default as OperacioneDePrestamos} from "./OperacioneDePrestamos"
