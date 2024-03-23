f071c448ca6a29cc7dc4bd4059aa0136288044d7
Ervigio <ejadd_75@hotmail.com>
2024-03-23 22:18:57 +0000

    Modificar contenido y estilo del componente Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="border-t py-4 px-3 flex justify-center bg-gradient-to-r from-orange-400 to-pink-600 shadow-xs">
+    <footer className="border-t py-4 px-3 flex items-center justify-around bg-emerald-700 shadow">
       <p>
-        Derechos de autor &copy;{new Date().getFullYear()} Blockmaker Academy
+        Derechos de autor &copy;{new Date().getFullYear()} Ervigio Javier Díaz Díaz        
       </p>
+      <div className="flex items-center">
+      <p>Trabajo de fin de Máster para Blockmaker Academy&reg;</p>
+      <img src="./blockmaker-small-logo.png" alt="logotipo de Blockmaker Academy" width={40}/>
+      </div>
+
     </footer>
   );
 }
