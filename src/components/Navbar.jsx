3972a52494a044083df7c30a31e818eedad39dec
Ervigio <ejadd_75@hotmail.com>
2024-03-23 14:26:39 +0000

    Agregar diseño responsivo a componente Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,31 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className=" ">
+    <nav>
       <ul className="flex ">
         <li className="px-4 ">
-          <Link to="/">Inicio y Autenticación</Link>
+          <Link to="/" className="hidden md:block">
+            Inicio y Autenticación
+          </Link>
+          <Link to="/" className="md:hidden">
+            Inicio
+          </Link>
         </li>
         <li className="px-4  ">
-          <Link to="/usuarios">Gestión de Usuarios</Link>
+          <Link to="/usuarios" className="hidden md:block">
+            Gestión de Usuarios
+          </Link>
+          <Link to="/usuarios" className="md:hidden">
+            Usuarios
+          </Link>
         </li>
         <li className="px-4 ">
-          <Link to="/prestamos">Operaciones de Préstamos</Link>
+          <Link to="/prestamos" className="hidden md:block">
+            Operaciones de Préstamos
+          </Link>
+          <Link to="/prestamos" className="md:hidden">
+            Préstamos
+          </Link>
         </li>
       </ul>
     </nav>
