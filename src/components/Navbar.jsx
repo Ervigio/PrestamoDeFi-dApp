f95ff16be35eafa204287eeeb55ef8d390acf7ab
Ervigio <ejadd_75@hotmail.com>
2024-03-23 22:23:10 +0000

    Modificar pading botones/etiquetas li

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
           </Link>
         </li>
 
-        <li className="p-1">
+        <li className="py-1">
           <Link to="/usuarios" className="hidden md:block">
             <button className="border-r px-2 ">Gestión de Usuarios</button>
           </Link>
@@ -22,7 +22,7 @@ export default function Navbar() {
           </Link>
         </li>
 
-        <li className="p-1">
+        <li className="py-1">
           <Link to="/prestamos" className="hidden md:block">
             <button className="px-2 ">Operaciones de Préstamos</button>
           </Link>
