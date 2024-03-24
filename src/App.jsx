5e699d61824d83e5307ac4da8ead3e8ed661d3fa
Ervigio <ejadd_75@hotmail.com>
2024-03-24 03:18:58 +0000

    Envolver la aplicación con: - WagmiConfig importado de wagmi - ConnectProvider importado de connectkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { AppLayout, Rutas } from "./components/ui/layaouts";
+import { WagmiConfig } from "wagmi";
+import { ConnectKitProvider } from "connectkit";
+
+import { config } from "./config/wagmi";
+import { InicioYAutenticacion } from "./pages";
 
 function App() {
   return (
-    <AppLayout>
-      <Rutas />
-    </AppLayout>
+    <WagmiConfig config={config}>
+      <ConnectKitProvider mode="auto">
+        <AppLayout>
+          <Rutas />
+          <InicioYAutenticacion />
+        </AppLayout>
+      </ConnectKitProvider>
+    </WagmiConfig>
   );
 }
 
