8d9db07c12e75176d930410cef1c11c7003e9739
Ervigio <ejadd_75@hotmail.com>
2024-03-24 14:35:30 +0000

    Mejorar diseño responsivo, cambiando tamño de texto a xs para pantallas reducidas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,23 @@
 export default function Footer() {
   return (
-    <footer className="border-t py-4 px-3 flex items-center justify-around bg-sky-900">
-      <p className="text-teal-100">
-        Derechos de autor &copy;{new Date().getFullYear()} Ervigio Javier Díaz Díaz        
-      </p>
-      <div className="flex items-center">
-      <p className="text-teal-100">Trabajo de Fin de Máster para Blockmaker Academy&reg;</p>
-      <img src="./blockmaker-small-logo.png" alt="logotipo de Blockmaker Academy" width={40}/>
+    <footer className="border-t py-4 px-3 flex items-center justify-around bg-sky-900 md:tex-xs">
+      <div>
+        <p className="text-teal-100">
+          Derechos de autor &copy;{new Date().getFullYear()} Ervigio Javier Díaz
+          Díaz
+        </p>
       </div>
 
+      <div className="flex items-center">
+        <p className="text-teal-100">
+          Trabajo de Fin de Máster para Blockmaker Academy&reg;
+        </p>
+        <img
+          src="./blockmaker-small-logo.png"
+          alt="logotipo de Blockmaker Academy"
+          width={40}
+        />
+      </div>
     </footer>
   );
 }
