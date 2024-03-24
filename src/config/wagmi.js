976fdad255b55f702f5431a907e74ae820caf538
Ervigio <ejadd_75@hotmail.com>
2024-03-24 03:13:51 +0000

    Añadir carpeta config y configurar wagmi

---
+++ src/config/wagmi.js
@@ -0,0 +1,23 @@
+import { createConfig } from "wagmi";
+import { getDefaultConfig } from "connectkit";
+import { sepolia } from "wagmi/chains";
+
+export const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: import.meta.env.VITE_ALCHEMY_ID, // or infuraId
+    walletConnectProjectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
+
+    // Required
+    appName: "Erv Préstamo DeFi DApp",
+
+    chains: [sepolia],
+
+    // Optional
+    //appDescription: "Your App Description",
+    //appUrl: "https://family.co", // your app's url
+    //appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  })
+);
+
+export default config;
