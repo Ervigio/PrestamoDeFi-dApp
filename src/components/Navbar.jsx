0b99459285e078150f32d828a0995a0aaf692201
Ervigio <ejadd_75@hotmail.com>
2024-03-24 14:24:58 +0000

    Corregir diseño responsivo componente  Navbar en Header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,36 +2,36 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="hidden sm:block">
       <ul className="flex ">
         <li className="p-1">
-          <Link to="/" className="hidden md:block">
+          <Link to="/" className="hidden lg:block">
             <button className="border-r px-2">Inicio y Autenticación</button>
           </Link>
-          <Link to="/" className="md:hidden">
+          <Link to="/" className="lg:hidden">
             <button className="border-r px-2">Inicio</button>
           </Link>
         </li>
 
         <li className="py-1">
-          <Link to="/usuarios" className="hidden md:block">
+          <Link to="/usuarios" className="hidden lg:block">
             <button className="border-r px-2 ">Gestión de Usuarios</button>
           </Link>
-          <Link to="/usuarios" className="md:hidden">
+          <Link to="/usuarios" className="lg:hidden">
             <button className="border-r px-2 ">Usuarios</button>
           </Link>
         </li>
 
         <li className="py-1">
-          <Link to="/prestamos" className="hidden md:block">
+          <Link to="/prestamos" className="hidden lg:block">
             <button className="px-2 ">Operaciones de Préstamos</button>
           </Link>
 
-          <Link to="/prestamos" className="md:hidden">
+          <Link to="/prestamos" className="lg:hidden">
             <button className="px-2 ">Préstamos</button>
           </Link>
         </li>
       </ul>
-    </nav>
+    </nav>    
   );
 }
