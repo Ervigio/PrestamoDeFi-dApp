99715255ea500f4a981e1d63c5b7f3ff552122ec
Ervigio <ejadd_75@hotmail.com>
2024-03-25 20:52:46 +0000

    Añadir prstamoDeFiABI.js

---
+++ src/contracts/ABIs/prestamoDeFiABI.js
@@ -0,0 +1,340 @@
+export const prestamoDeFiABI = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    name: "EmpleadoPrestamistaActivado",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "prestatario",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "monto",
+        type: "uint256",
+      },
+    ],
+    name: "GarantiaLiquidado",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "prestatario",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "monto",
+        type: "uint256",
+      },
+    ],
+    name: "PrestamoAprobado",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "prestatario",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "monto",
+        type: "uint256",
+      },
+    ],
+    name: "PrestamoReembolsado",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "prestatario",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "monto",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "plazo",
+        type: "uint256",
+      },
+    ],
+    name: "SolicitudPrestamo",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "nuevoCliente",
+        type: "address",
+      },
+    ],
+    name: "altaCliente",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "nuevoPrestamista",
+        type: "address",
+      },
+    ],
+    name: "altaPrestamista",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "prestatario_",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "id_",
+        type: "uint256",
+      },
+    ],
+    name: "aprobarPrestamo",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "clienteRegistrado_",
+        type: "address",
+      },
+    ],
+    name: "depositarGarantia",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "empleadosPrestamista",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "prestatario_",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "id_",
+        type: "uint256",
+      },
+    ],
+    name: "liquidarGarantia",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "prestatario_",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "id_",
+        type: "uint256",
+      },
+    ],
+    name: "obtenerDetalleDePrestamo",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256",
+            name: "id",
+            type: "uint256",
+          },
+          {
+            internalType: "address",
+            name: "prestatario",
+            type: "address",
+          },
+          {
+            internalType: "uint256",
+            name: "monto",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "plazo",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "tiempoSolicitud",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "tiempoLimite",
+            type: "uint256",
+          },
+          {
+            internalType: "bool",
+            name: "aprobado",
+            type: "bool",
+          },
+          {
+            internalType: "bool",
+            name: "reembolsado",
+            type: "bool",
+          },
+          {
+            internalType: "bool",
+            name: "liquidado",
+            type: "bool",
+          },
+        ],
+        internalType: "struct PrestamoDeFi.Prestamo",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "prestatario_",
+        type: "address",
+      },
+    ],
+    name: "obtenerPrestamosPorPrestatario",
+    outputs: [
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "id_",
+        type: "uint256",
+      },
+    ],
+    name: "reembolsarPrestamo",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "socioPrincipal",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "monto_",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "plazo_",
+        type: "uint256",
+      },
+    ],
+    name: "solicitarPrestamos",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
