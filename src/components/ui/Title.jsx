4ac674e82e007b76bf22a7dfc9e3f6a9bdc7bab3
Ervigio <ejadd_75@hotmail.com>
2024-03-26 01:21:42 +0000

    Corregir estilo componente Title para hacerlo responsivo

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -1,11 +1,11 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
-export default function Title({children}){
-    return(
-        <h3 className="text-xl text-sky-950 font-bold">{children}</h3>
-    )
+export default function Title({ children }) {
+  return (
+    <h3 className="text-sm sm:text-xl text-sky-950 font-bold">{children}</h3>
+  );
 }
 
-Title.propTypes={
-    children: PropTypes.string.isRequired
-}
+Title.propTypes = {
+  children: PropTypes.string.isRequired,
+};
