cd98be19f894b9b63777f6147521820950b75cb5
Ervigio <ejadd_75@hotmail.com>
2024-03-26 01:29:58 +0000

    Importar componentes Alta Prestamista y Alta Cliente en el componente página GestionDeUsuarios

--- src/pages/GestionDeUsuarios.jsx
+++ src/pages/GestionDeUsuarios.jsx
@@ -1,7 +1,19 @@
+import { AltaPrestamista, AltaCliente } from "../components";
+
 export default function GestionDeUsuarios() {
   return (
-    <div>
-      <h3>Ésta es la Página 2</h3>
-    </div>
+    <section className="flex flex-col gap-16 items-center">
+      <div className="text-5xl pt-10 font-thin text-center text-sky-950">
+        <h2>Gestión de Usuarios</h2>
+      </div>
+      <div>
+        <AltaPrestamista />
+      </div>
+      <div>
+        <AltaCliente />
+      </div>
+     
+
+    </section>
   );
 }
