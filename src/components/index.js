47dd7865a8891d37503727e88f4081b97f0b7603
Ervigio <ejadd_75@hotmail.com>
2024-03-26 01:28:56 +0000

    Incluir componente AltaPRestamista y AltaCliente en el índice de components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
+export { default as AltaPrestamista } from "./AltaPrestamista";
+export { default as AltaCliente } from "./AltaCliente";
