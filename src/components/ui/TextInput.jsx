37c03e66748c743b67f489e5bf3c6fead38b35b6
Ervigio <ejadd_75@hotmail.com>
2024-03-26 01:21:22 +0000

    Corregir estilo componente TextInput para hacerlo responsivo

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -15,9 +15,9 @@ export default function TextInput({
       placeholder={placeholder}
       disabled={disabled}
       className="bg-cyan-100 border border-sky-700  text-sky-900 font-semibold
-      rounded-lg w-80 focus:ring-2 focus:ring-sky-600 focus:border-transparent
+      rounded-lg w-72 sm:w-96 focus:ring-2 focus:ring-sky-600 focus:border-transparent
       transition duration-300 disabled:cursor-not-allowed disabled:opacity-50
-      outline-none py-2 px-3"
+      outline-none py-2 px-3 text-center text-xs sm:text-sm"
     />
   );
 }
