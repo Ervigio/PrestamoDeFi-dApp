a87cefde5c7cc298fc42bb56ea82be192b25f757
Ervigio <ejadd_75@hotmail.com>
2024-04-01 00:28:43 +0100

    Preparar estructura inicial de página "Operaciones de Préstmos": 1.- Cambiando etiqueta h3 por h2 así como el contenido de la etiqueta.
    2.- Añadiendo dos secciones; una para los componentes relativos a las funciones soloEmpleadoPrestamista y la otra para los componentes  relativos a las funciones soloClienteRegistrado.

--- src/pages/OperacionesDePrestamos.jsx
+++ src/pages/OperacionesDePrestamos.jsx
@@ -1,7 +1,44 @@
 export default function OperacionesDePrestamos() {
   return (
     <div>
-      <h3>Ésta es la Página 3</h3>
+      <div className="text-5xl p-5 gap-6 font-thin text-center text-sky-950">
+        <h2>Operaciones de Préstamos</h2>
+        <hr />
+      </div>
+
+      <div className="flex justify-evenly">
+        {/*Clientes*/}
+
+        <section className="grid gap-10 items-center">
+          <div>
+            
+            <p>Componente Solicitar Préstamo</p>
+          </div>
+
+          <div>
+            <p>Componente Reembolsar Préstamo</p>
+          </div>
+
+          <div>
+            <p>Componente Préstamos Clientes</p>
+          </div>
+        </section>
+
+        {/*EmpleadoPrestamista*/}
+
+        <section className="grid gap-10 items-center">
+          <div>
+            <p>Componente Aprobar Préstamo</p>
+          </div>
+
+          <div>
+            <p>Componente Liquidar Garantía</p>
+          </div>
+          <div>
+            <p>Componente Préstamos Clientes</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
