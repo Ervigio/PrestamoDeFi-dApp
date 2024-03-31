140e3955751fe4bb3fab2b4b9056a995b8460524
Ervigio <ejadd_75@hotmail.com>
2024-03-31 14:40:47 +0100

    Parsear el componente Home tras commint anterior

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,4 @@
-import {
-
-  Title,
-} from "../components/ui";
+import { Title } from "../components/ui";
 
 export default function Home() {
   return (
@@ -29,22 +26,20 @@ export default function Home() {
         </div>
       </section>
 
-
-
       <section className="flex flex-col gap-16 items-center">
         <div className="grid gap-3 px-5 bg-white p-4 border shadow rounded-lg text-sm w-72 sm:min-w-[40vw]">
           <div>
             <div>
-              <Title>Al margen de la Banca Tradicional. Banca para todos.</Title>
+              <Title>
+                Al margen de la Banca Tradicional. Banca para todos.
+              </Title>
             </div>
 
             <p className="py-3 text-xs sm:text-sm ">
-              No todo el mundo puede acceder a los servicios
-              financieros. Sin embargo, todo lo que necesita para acceder a
-              Ethereum y a los productos de préstamos, toma de préstamos y
-              ahorros integrados en Ethereum es una conexión a Internet.
-            
-
+              No todo el mundo puede acceder a los servicios financieros. Sin
+              embargo, todo lo que necesita para acceder a Ethereum y a los
+              productos de préstamos, toma de préstamos y ahorros integrados en
+              Ethereum es una conexión a Internet.
             </p>
           </div>
         </div>
@@ -54,19 +49,18 @@ export default function Home() {
         <div className="grid gap-3 px-5 bg-white p-4 border shadow rounded-lg text-sm w-72 sm:min-w-[40vw]">
           <div>
             <div>
-              <Title>Tecnología Blokchain.  </Title>
+              <Title>Tecnología Blokchain. </Title>
             </div>
 
             <p className="py-3 text-xs sm:text-sm ">
-            Cualquiera puede interactuar con la red Ethereum o 
-            construir aplicaciones en ella. Esto le permite controlar 
-            sus propios activos e identidad, en lugar de estar controlados 
-            por unas cuantas megacorporaciones.
+              Cualquiera puede interactuar con la red Ethereum o construir
+              aplicaciones en ella. Esto le permite controlar sus propios
+              activos e identidad, en lugar de estar controlados por unas
+              cuantas megacorporaciones.
             </p>
           </div>
         </div>
       </section>
     </div>
-    
   );
 }
