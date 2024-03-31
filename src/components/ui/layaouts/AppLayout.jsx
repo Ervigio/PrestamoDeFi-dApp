6cfa6642ce187219250d4ba3707ef7034dfddba0
Ervigio <ejadd_75@hotmail.com>
2024-03-31 14:27:49 +0100

    - Añadir componente Navbar envolviéndolo en una etiqueta div para dotarlo de diseño responsivo mostarndo el menú de navegación sólo hasta ancho de pantalla  sm de Tailwind (640px). - Ajustar tamaño de main en pantalla

--- src/components/ui/layaouts/AppLayout.jsx
+++ src/components/ui/layaouts/AppLayout.jsx
@@ -1,12 +1,15 @@
-import { Footer, Header } from "../../../components";
+import { Footer, Header, Navbar } from "../../../components";
 import { PropTypes } from "prop-types";
 
 export default function AppLayout({ children }) {
   return (
     <>
+    
       <Header />
-      <main className="min-h-[85vh] bg-teal-50">{children}</main>
+      <main className="min-h-[82vh] sm:min-h-[85vh] bg-teal-50" >{children}</main>
+      <div className="bg-gradient-to-r from-sky-600 to-cyan-200 py-2 px-3 content-end border-b shadow-md sm:hidden"><Navbar/></div>
       <Footer />
+      
     </>
   );
 }
