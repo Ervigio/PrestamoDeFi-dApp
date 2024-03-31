517c28c4d0aef253bfbbf967419d92b17d48b198
Ervigio <ejadd_75@hotmail.com>
2024-04-01 00:41:11 +0100

    Actualizar componente AltaCliente dotándolo de lógica implementendo los Hooks de Wagmi: usePrepareContractWrite, useWaitForTransaction, useContractWrite. Añadiendo feedback al usuario usando lógica y también la funcionalidad de React Hot Toast

--- src/components/AltaCliente.jsx
+++ src/components/AltaCliente.jsx
@@ -1,17 +1,69 @@
 import { Title, TextInput, Button } from "./ui";
+import { prestamoDeFiABI } from "../contracts/ABIs";
+import {
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import { useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
 
 export default function AltaCliente() {
+  const [address, setAddress] = useState(" ");
+
+  const { config } = usePrepareContractWrite({
+    address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
+    abi: prestamoDeFiABI,
+    functionName: "altaCliente",
+    enabled: address,
+    args: [address],
+  });
+
+  const { data, write } = useContractWrite(config);
+
+  const {
+    isLoading: isTransactionLoading,
+    isSuccess: isTransactionSuccess,
+    isError: isTransactionError,
+  } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+  // Para recoger el valor del input en la sección Alta Cliente
+  const handleAddressInputChange = (event) => {
+    setAddress(event.target.value);
+  };
+
+  useEffect(() => {
+    if (isTransactionSuccess) {
+      toast.success("La transacción se ha completado con éxito");
+      setAddress(" ");
+    }
+    if (isTransactionError) {
+      toast.error("La transacción se ha fallado");
+    }
+  }, [isTransactionSuccess, isTransactionError]);
+
   return (
     <section className="grid gap-3 px-5 bg-white p-4 border shadow rounded-lg text-sm w-fit">
       <Title>Alta Cliente</Title>
       <form action="">
         <TextInput
-          label="Address"
+          label="address"
           placeholder="Address Nuevo Cliente 0x..."
-          type="string"
+          onChange={handleAddressInputChange}
+          value={address}
         />
       </form>
-      <Button>Alta Cliente</Button>
+      <Button
+        onClick={() => write?.()}
+        disabled={!address || isTransactionLoading}
+        isLoading={isTransactionLoading}
+      >
+        {isTransactionLoading
+          ? "Gestionando Alta Nuevo Cliente"
+          : "Alta Cliente"}
+      </Button>
     </section>
   );
 }
