eef7991a7727dce790bb0172f02718a7e264a419
Ervigio <ejadd_75@hotmail.com>
2024-03-31 14:35:28 +0100

    - Añadir contenido controlándolo por medio de un div general que incluye el título de la página h1 y tres secciones diferenciadas en tres cajas de texto con título y descripción propios, añdiendo diseño responsivo a cada elemento d ela página

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,76 @@
-import { LoadingSpinner, Button, ErrorInfo, TextInput, Title } from "../components/ui";
+import {
+  LoadingSpinner,
+  Button,
+  ErrorInfo,
+  TextInput,
+  Title,
+} from "../components/ui";
+import { Navbar } from "../components";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="grid gap-6 ">
       <h1 className="text-5xl p-5  font-thin text-center text-sky-950">
         Préstamo DeFi
       </h1>
       <hr />
-      <div>
-        <p className="p-5 text-sky-700">
-          ¿Qué significa la palabra DeFi? <br />
-          Finanzas descentralizadas, comúnmente conocidas como «DeFi», es el
-          término que se utiliza para describir un sistema de servicios
-          financieros basado en la cadena de bloques que elimina la necesidad de
-          que las agencias gubernamentales aprueben las transacciones.
-        </p>
-      </div>
 
-      <div>
-        <LoadingSpinner className="h-8 w-8" />
-      </div>
-      <div>
-        <Button disabled={false}>Alta Prestamista</Button>
-      </div>
-      <div>
-        <ErrorInfo message="Error: Internal Server Error" />
-      </div>
-      <div>
-        <TextInput placeholder="Introduce los datos" disabled={false}/>
-      </div>
-      <div>
-        <Title>Título de este componente</Title>
-      </div>
+      <section className="flex flex-col gap-16 items-center">
+        <div className="grid gap-3 px-5 bg-white p-4 border shadow rounded-lg w-72 sm:min-w-[60vw]">
+          <div>
+            <div>
+              <Title>Finanzas Descentralizadas</Title>
+            </div>
+
+            <p className="py-3 text-xs sm:text-sm ">
+              Finanzas descentralizadas, comúnmente conocidas como «DeFi», es el
+              término que se utiliza para describir un sistema de servicios
+              financieros basado en la cadena de bloques que elimina la
+              necesidad de que las agencias gubernamentales aprueben las
+              transacciones.
+            </p>
+          </div>
+        </div>
+      </section>
+
+
+
+      <section className="flex flex-col gap-16 items-center">
+        <div className="grid gap-3 px-5 bg-white p-4 border shadow rounded-lg text-sm w-72 sm:min-w-[60vw]">
+          <div>
+            <div>
+              <Title>Al margen de la Banca Tradicional. Banca para todos.</Title>
+            </div>
+
+            <p className="py-3 text-xs sm:text-sm ">
+              No todo el mundo puede acceder a los servicios
+              financieros. Sin embargo, todo lo que necesita para acceder a
+              Ethereum y a los productos de préstamos, toma de préstamos y
+              ahorros integrados en Ethereum es una conexión a Internet.
+            
+
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-16 items-center">
+        <div className="grid gap-3 px-5 bg-white p-4 border shadow rounded-lg text-sm w-72 sm:min-w-[60vw]">
+          <div>
+            <div>
+              <Title>Tecnología Blokchain.  </Title>
+            </div>
+
+            <p className="py-3 text-xs sm:text-sm ">
+            Cualquiera puede interactuar con la red Ethereum o 
+            construir aplicaciones en ella. Esto le permite controlar 
+            sus propios activos e identidad, en lugar de estar controlados 
+            por unas cuantas megacorporaciones.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
+    
   );
 }
