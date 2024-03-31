f7ccdfdc1e17d4f6bb02d8475dbe50294d65b46e
Ervigio <ejadd_75@hotmail.com>
2024-04-01 00:38:00 +0100

    Implementar funcionalidades de la librería React Hot Toast en el componente App

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import { ConnectKitProvider } from "connectkit";
 import { config } from "./config/wagmi";
 import { InicioYAutenticacion } from "./pages";
 
+import { Toaster } from "react-hot-toast";
+
 function App() {
   return (
     <WagmiConfig config={config}>
+      <Toaster position="bottom-right" />
       <ConnectKitProvider mode="auto">
         <AppLayout>
-          <InicioYAutenticacion/>
+          <InicioYAutenticacion />
           <Rutas />
         </AppLayout>
       </ConnectKitProvider>
