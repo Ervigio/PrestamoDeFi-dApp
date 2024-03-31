954c927b90d4bd9d83ae5598cdeaf752e0f562f5
Ervigio <ejadd_75@hotmail.com>
2024-03-31 14:23:18 +0100

    Corregir tamaño de textos, insertar saltos de línea y mejorar diseño responsivo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 export default function Footer() {
   return (
-    <footer className="border-t py-4 px-3 flex items-center justify-around bg-sky-900 md:tex-xs">
+    <footer className="border-t py-4 px-3 flex items-center justify-evenly bg-sky-900">
       <div>
-        <p className="text-teal-100">
-          Derechos de autor &copy;{new Date().getFullYear()} Ervigio Javier Díaz
+        <p className="text-teal-100 text-xs sm:text-base ">
+          Derechos de autor &copy;{new Date().getFullYear()} <br className="lg:hidden"/> Ervigio Javier Díaz
           Díaz
         </p>
       </div>
 
-      <div className="flex items-center">
+      <div className="flex items-center gap-3 text-xs sm:text-base ">
         <p className="text-teal-100">
-          Trabajo de Fin de Máster para Blockmaker Academy&reg;
+          Trabajo Fin de Máster para <br className="lg:hidden"/>Blockmaker Academy&reg;
         </p>
         <img
           src="./blockmaker-small-logo.png"
