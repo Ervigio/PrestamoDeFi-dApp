7a6000c6f3edb8eb43646cd5d73e1fd4586a2537
Ervigio <ejadd_75@hotmail.com>
2024-04-01 00:22:56 +0100

    Reestructurar estructura de la página sacando la etiqueta de título h2 de la etiqueta section e incluyéndola dentro de una etiqueta div a la que se le ha añadido los estilos className correspondientes

--- src/pages/GestionDeUsuarios.jsx
+++ src/pages/GestionDeUsuarios.jsx
@@ -2,18 +2,19 @@ import { AltaPrestamista, AltaCliente } from "../components";
 
 export default function GestionDeUsuarios() {
   return (
-    <section className="flex flex-col gap-16 items-center">
+    <div>
       <div className="text-5xl pt-10 font-thin text-center text-sky-950">
         <h2>Gestión de Usuarios</h2>
+        <hr />
       </div>
-      <div>
-        <AltaPrestamista />
-      </div>
-      <div>
-        <AltaCliente />
-      </div>
-     
-
-    </section>
+      <section className="flex flex-col pt-10 gap-16 items-center">
+        <div>
+          <AltaPrestamista />
+        </div>
+        <div>
+          <AltaCliente />
+        </div>
+      </section>
+    </div>
   );
 }
