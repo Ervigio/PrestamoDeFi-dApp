186a41a92642582783577d1236004e0e371f3fcf
Ervigio <ejadd_75@hotmail.com>
2024-04-01 23:46:31 +0100

    Cambiar tamaño del LoadingSpinner adecuándolo al tamaño de componente Button

--- src/components/ui/LoadingSpinner.jsx
+++ src/components/ui/LoadingSpinner.jsx
@@ -6,7 +6,7 @@ export default function LoadingSpinner({ className }) {
       <div role="status">
         <svg
           aria-hidden="true"
-          className={`w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-sky-500 ${className}`}
+          className={`w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-sky-500  ${className}`}
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
