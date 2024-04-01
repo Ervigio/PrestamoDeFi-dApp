fc2f28d8166cce5dad8e26f9d6e9a80e37d8c4dd
Ervigio <ejadd_75@hotmail.com>
2024-04-01 19:17:33 +0100

    Actualizar componenteAlta Prestamista implementando hooks: usePrepareWrite, useContractWrite, useWaitForTransaction, useState, useEffect, implemenmtando la libreíra de react React-Hot-Toast

--- src/components/AltaPrestamista.jsx
+++ src/components/AltaPrestamista.jsx
@@ -1,7 +1,12 @@
 import { Title, TextInput, Button } from "./ui";
 import { prestamoDeFiABI } from "../contracts/ABIs";
-import { useContractWrite, usePrepareContractWrite } from "wagmi";
-import { useState } from "react";
+import {
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import { useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
 
 export default function AltaPrestamista() {
   const [address, setAddress] = useState("");
@@ -10,14 +15,35 @@ export default function AltaPrestamista() {
     address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
     abi: prestamoDeFiABI,
     functionName: "altaPrestamista",
+    enabled: address,
     args: [address],
   });
 
   const { data, write } = useContractWrite(config);
 
+  const {
+    isLoading: isTransactionLoading,
+    isSuccess: isTransactionSuccess,
+    isError: isTransactionError,
+  } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
   const handleAddressInputChange = (event) => {
     setAddress(event.target.value);
   };
+
+  useEffect(() => {
+    if (isTransactionSuccess) {
+      toast.success("Prestamista dado de alta con éxito");
+      setAddress("");
+    }
+    if (isTransactionError) {
+      toast.error(
+        "La transacción ha fallado, por favor, revisa los datos de entrada"
+      );
+    }
+  }, [isTransactionSuccess, isTransactionError]);
 
   return (
     <section className="grid gap-3 px-5 bg-white p-4 border shadow rounded-lg text-sm w-fit">
@@ -27,10 +53,18 @@ export default function AltaPrestamista() {
           label="address"
           placeholder="Address Nuevo Empleado Prestamista 0x..."
           onChange={handleAddressInputChange}
+          value={address}
         />
       </form>
-      <Button onClick={() => write?.()}>Alta Prestamista</Button>
-      <p></p>
+      <Button
+        onClick={() => write?.()}
+        disabled={!address || isTransactionLoading}
+        isLoading={isTransactionLoading}
+      >
+        {isTransactionLoading
+          ? "Tramitando Alta Nuevo Prestamista"
+          : "Alta Prestamista"}
+      </Button>
     </section>
   );
 }
