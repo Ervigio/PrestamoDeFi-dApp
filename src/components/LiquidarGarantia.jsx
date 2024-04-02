0b696d8bdc2a21dc8f0bd01a51244e289f57a151
Ervigio <ejadd_75@hotmail.com>
2024-04-02 19:15:40 +0100

    Crear el componente LiquidarGarantia

---
+++ src/components/LiquidarGarantia.jsx
@@ -0,0 +1,26 @@
+//
+import { Title, TextInput, Button } from "./ui";
+
+export default function LiquidarGarantia() {
+  return (
+    <section className="grid gap-3">
+      <Title>Liquidar Garantia</Title>
+
+      <form className="grid gap-1 border-2 rounded-lg">
+        <div className="grid gap-3 p-2">
+          <TextInput
+            type="texto"
+            placeholder="Introduce dirección Monedero Prestatario (0x.....)"
+          />
+          <TextInput
+            type="number"
+            placeholder="Introduce el ID del Préstamo a Liquidar"
+          />
+        </div>
+        <div className="grid p-2">
+          <Button>Liquidar Garantia</Button>
+        </div>
+      </form>
+    </section>
+  );
+}
