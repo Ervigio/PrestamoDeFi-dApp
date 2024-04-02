579256a49b2a03eb9e68dd9476a1b12fc9f28662
Ervigio <ejadd_75@hotmail.com>
2024-04-02 19:14:42 +0100

    Crear componente AprobarPrestamo

---
+++ src/components/AprobarPrestamo.jsx
@@ -0,0 +1,25 @@
+import { Title, TextInput, Button } from "./ui";
+
+export default function AprobarPrestamo() {
+  return (
+    <section className="grid gap-3">
+      <Title>Aprobar Péstamo</Title>
+
+      <form className="grid gap-1 border-2 rounded-lg">
+        <div className="grid gap-3 p-2">
+          <TextInput
+            type="texto"
+            placeholder="Introduce dirección Monedero Solicitante (0x.....)"
+          />
+          <TextInput
+            type="number"
+            placeholder="Indica el Monto en Ether Aprobado"
+          />
+        </div>
+        <div className="grid p-2">
+          <Button>Aprobar Préstamo</Button>
+        </div>
+      </form>
+    </section>
+  );
+}
