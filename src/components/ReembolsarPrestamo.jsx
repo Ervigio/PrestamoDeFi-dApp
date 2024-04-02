7ee974e3ffe4787d6afb665ecbf4c2f1e878c53f
Ervigio <ejadd_75@hotmail.com>
2024-04-02 19:15:14 +0100

    Crear el componente ReembolsarPRestamo

---
+++ src/components/ReembolsarPrestamo.jsx
@@ -0,0 +1,19 @@
+import { Title, TextInput, Button } from "./ui";
+
+export default function ReembolsarPrestamo() {
+  return (
+    <section className="grid gap-3">
+      <Title>Reembolsar Péstamo</Title>
+
+      <form className="grid gap-3">
+        <div className="grid gap-3 border-2 rounded-lg p-2">
+          <TextInput
+            type="number"
+            placeholder="Indica el ID del Préstamo a Reembolsar"
+          />
+          <Button>Reembolsar Préstamo</Button>
+        </div>
+      </form>
+    </section>
+  );
+}
