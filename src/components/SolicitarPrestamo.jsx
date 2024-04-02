ce6726c3cdeab63af1933411a91c245442ec3c83
Ervigio <ejadd_75@hotmail.com>
2024-04-02 19:13:50 +0100

    Actualizar componente SolicitarPrestamo modificando formualrios acorde a las necesidades de las funciones SOLIDITY del contrato implicadas en este componente: depositarGarantia y solicitarPRestamos

--- src/components/SolicitarPrestamo.jsx
+++ src/components/SolicitarPrestamo.jsx
@@ -5,14 +5,30 @@ export default function SolicitarPrestamo() {
     <section className="grid gap-3">
       <Title>Solicitar Péstamo</Title>
 
-      <form className="grid gap-3">
-        <div className="grid gap-3 border-2 rounded-lg p-2">
-          <TextInput />
-          <Button>Ingresar Saldo Garantía</Button>
+      <form className="grid gap-1 border-2 rounded-lg">
+        <div className="grid gap-3 p-2">
+          <TextInput
+            type="number"
+            placeholder="Paso 1: Deposita Garantía (100% Préstamo Solicitado)"
+          />
         </div>
+        <div className="grid p-2">
+          <Button>Depositar Saldo Garantía</Button>
+        </div>
+      </form>
 
-        <div className="grid gap-3 border-2 rounded-lg p-2">
-          <TextInput />
+      <form className="grid gap-1 border-2 rounded-lg">
+        <div className="grid gap-3 p-2">
+          <TextInput
+            type="number"
+            placeholder="Paso 2: Indica Monto en Ether que se Solicita"
+          />
+          <TextInput
+            type="number"
+            placeholder="Paso 3: Indica Plazo de Devolución (días NATURALES)"
+          />
+        </div>
+        <div className="grid p-2">
           <Button>Enviar Solicitud</Button>
         </div>
       </form>
