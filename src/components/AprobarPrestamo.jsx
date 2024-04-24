5d032593345618bea073c2457c3d93799e7f0c82
Ervigio <ejadd_75@hotmail.com>
2024-04-24 22:44:17 +0100

    Actualizar componente AprobarPrestamo implementando y desarrollando los hooks usePrepareContractWrite, useContractWrite y useWaitForTransaction de Wagmi; useState y useEffect de react e implementando el toast …

--- src/components/AprobarPrestamo.jsx
+++ src/components/AprobarPrestamo.jsx
@@ -1,6 +1,53 @@
 import { Title, TextInput, Button } from "./ui";
+import { prestamoDeFiABI } from "../contracts/ABIs";
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import { useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
 
 export default function AprobarPrestamo() {
+  const [address, setAddress] = useState("");
+  const [id, setId] = useState("");
+
+  const { config } = usePrepareContractWrite({
+    address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
+    abi: prestamoDeFiABI,
+    functionName: "aprobarPrestamo",
+    args: [address, id],
+  });
+
+  const { data, write } = useContractWrite(config);
+
+  const {
+    isLoading: isTransactionLoading,
+    isSuccess: isTransactionSuccess,
+    isError: isTransactionError,
+  } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+  const handleAddressInputChange = (event) => {
+    setAddress(event.target.value);
+  };
+  const handleIdInputChange = (event) => {
+    setId(event.target.value);
+  };
+
+  useEffect(() => {
+    if (isTransactionSuccess) {
+      toast.success("El préstamo ha sido aprobado exitosamente");
+      setAddress("");
+    }
+    if (isTransactionError) {
+      toast.error(
+        "No se ha podidoaprobar el préstamo. Por favor, revisa los datos introducidos"
+      );
+    }
+  }, [isTransactionSuccess, isTransactionError]);
+
   return (
     <section className="grid gap-3">
       <Title>Aprobar Préstamo</Title>
@@ -10,14 +57,24 @@ export default function AprobarPrestamo() {
           <TextInput
             type="texto"
             placeholder="Introduce dirección Monedero Solicitante (0x.....)"
+            onChange={handleAddressInputChange}
           />
           <TextInput
             type="number"
             placeholder="Indica el Monto en Ether Aprobado"
+            onChange={handleIdInputChange}
           />
         </div>
         <div className="grid p-2">
-          <Button>Aprobar Préstamo</Button>
+          <Button
+            onClick={() => write?.()}
+            disabled={!address || !id || isTransactionLoading}
+            isLoading={isTransactionLoading}
+          >
+            {isTransactionLoading
+              ? "Aprobación en curso..."
+              : "Aprobar Préstamo"}
+          </Button>
         </div>
       </form>
     </section>
