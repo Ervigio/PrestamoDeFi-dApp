a459f8a8ace0ac40fc5f8d2357aff13bd3f8c3d4
Ervigio <ejadd_75@hotmail.com>
2024-04-24 23:26:19 +0100

    Actualizar componente LiquidarGarantia implementando y desarrollando los hooks usePrepareContractWrite, useContractWrite y useWaitForTransaction de Wagmi; useState y useEffect de react e implementado el toast from reacto-hot-toast

--- src/components/LiquidarGarantia.jsx
+++ src/components/LiquidarGarantia.jsx
@@ -1,7 +1,53 @@
-//
 import { Title, TextInput, Button } from "./ui";
+import { prestamoDeFiABI } from "../contracts/ABIs";
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import { useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
 
 export default function LiquidarGarantia() {
+  const [address, setAddress] = useState("");
+  const [id, setId] = useState("");
+
+  const { config } = usePrepareContractWrite({
+    address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
+    abi: prestamoDeFiABI,
+    functionName: "liquidarGarantia",
+    args: [address, id],
+  });
+
+  const { data, write } = useContractWrite(config);
+
+  const {
+    isLoading: isTransactionLoading,
+    isSuccess: isTransactionSuccess,
+    isError: isTransactionError,
+  } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+  const handleAddressInputChange = (event) => {
+    setAddress(event.target.value);
+  };
+  const handleIdInputChange = (event) => {
+    setId(event.target.value);
+  };
+
+  useEffect(() => {
+    if (isTransactionSuccess) {
+      toast.success("El depósito de garantía se ha liquidado exitosamente");
+      setAddress("");
+    }
+    if (isTransactionError) {
+      toast.error(
+        "No se ha podido liquidar el depósito de garantía. Por favor, revisa los datos introducidos"
+      );
+    }
+  }, [isTransactionSuccess, isTransactionError]);
+
   return (
     <section className="grid gap-3">
       <Title>Liquidar Garantía</Title>
@@ -11,14 +57,22 @@ export default function LiquidarGarantia() {
           <TextInput
             type="texto"
             placeholder="Introduce dirección Monedero Prestatario (0x.....)"
+            onChange={handleAddressInputChange}
           />
           <TextInput
             type="number"
             placeholder="Introduce el ID del Préstamo a Liquidar"
+            onChange={handleIdInputChange}
           />
         </div>
         <div className="grid p-2">
-          <Button>Liquidar Garantía</Button>
+          <Button
+            onclick={() => write?.()}
+            disabled={!address || !id || isTransactionLoading}
+            isLoading={isTransactionLoading}
+          >
+            {isTransactionLoading? "Liquidando el Depósito de Garantía":"Liquidar Depósito de Garantía"}
+          </Button>
         </div>
       </form>
     </section>
